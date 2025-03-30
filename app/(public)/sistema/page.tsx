cf46bafa045094715126cc4714d73471ebb88785
Samuel Zanini <samuelzanini.trabalho@gmail.com>
2025-03-30 18:36:52 -0300

    style: 💄 Finalizado as Páginas de "tipos" e "sistemas"
    Adicionados elementos visuais nas páginas e ajustes

--- app/(public)/sistema/page.tsx
+++ app/(public)/sistema/page.tsx
@@ -1,9 +1,140 @@
+"use client";
+
+import { motion } from "motion/react";
+
+import ElasticLine from "@/fancy/components/physics/elastic-line";
+import { Factory } from "lucide-react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function Sistema() {
+  const textVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.3,
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      },
+    }),
+  };
+
   return (
-    <main className="sm:ml-14 p-4">
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+    <main className="fixed inset-0 sm:ml-14 overflow-y-auto">
+      <motion.div
+        initial={{ opacity: 0, y: 200 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5 }}
+      >
+        <div className="relative w-full h-full flex flex-row items-center justify-center font-overused-grotesk text-foreground">
+          <div className="absolute left-0 top-0 w-full h-full px-6 sm:px-8 md:px-12 z-10">
+            <ElasticLine
+              releaseThreshold={50}
+              strokeWidth={1}
+              animateInTransition={{
+                type: "spring",
+                stiffness: 300,
+                damping: 30,
+                delay: 0.15,
+              }}
+            />
+          </div>
+          {/* This is just fluff for the demo */}
+          <div className="h-full flex flex-col py-6 w-full px-6 sm:px-8 md:px-12 font-light">
+            <div className="h-1/2 py-8 w-full items-end flex">
+              <motion.p
+                variants={textVariants}
+                initial="hidden"
+                animate="visible"
+                className="uppercase text-4xl sm:text-5xl md:text-6xl font-medium"
+                custom={0}
+              >
+                Funcionamento do Sistema
+              </motion.p>
+              <Factory className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 ml-auto text-orange-500" />
+            </div>
+            <div className="flex flex-row  pt-8 justify-between items-start gap-x-4">
+              <motion.p
+                variants={textVariants}
+                initial="hidden"
+                animate="visible"
+                className="w-1/3 uppercase md:text-7xl hidden md:block text-orange-500"
+                custom={0}
+              >
+                ⁜
+              </motion.p>
+              <motion.p
+                variants={textVariants}
+                initial="hidden"
+                animate="visible"
+                className="w-full md:w-2/3 sm:text-left text-base sm:text-xl md:text-2xl"
+                custom={1}
+              >
+                O sistema consiste em uma esteira separadora de materiais. Essa
+                esteira segrega as peças coloridas de acordo com sua cor,
+                tamanho e material, contabilizando cada uma delas! Segue abaixo
+                uma foto do sistema:
+              </motion.p>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 200 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.7 }}
+        className="flex justify-center items-center gap-4 py-7 mb-8"
+      >
+        <Carousel className="w-full max-w-xl">
+          <CarouselContent>
+            <CarouselItem>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-8">
+                    <span className="text-4xl font-semibold">
+                      substituir por imagem do sistema
+                    </span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-8">
+                    <span className="text-4xl font-semibold">
+                      substituir por imagem do sistema
+                    </span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-8">
+                    <span className="text-4xl font-semibold">
+                      substituir por imagem do sistema
+                    </span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </motion.div>
     </main>
   );
 }
--- app/(public)/tipos/page.tsx
+++ app/(public)/tipos/page.tsx
@@ -1,9 +1,127 @@
+"use client";
+
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+import StackingCards, {
+  StackingCardItem,
+} from "@/fancy/components/blocks/stacking-cards";
+import { MoveDown } from "lucide-react";
+import { motion } from "motion/react";
+
 export default function Sistema() {
+  const cards = [
+    {
+      bgColor: "bg-yellow-400/15 backdrop-blur-md",
+      title: "🍌 Peças Amarelas",
+      description: [
+        "As peças amarelas são compostas por 6 unidades, sendo elas:",
+        "⚡ 1 peça de metal pequena",
+        "⚡ 1 peça de metal média",
+        "⚡ 1 peça de metal grande",
+        "⚡ 1 peça de plástico pequena",
+        "⚡ 1 peça de plástico média",
+        "⚡ 1 peça de plástico grande",
+      ],
+      image:
+        "https://img.freepik.com/fotos-premium/fundo-de-textura-de-papel-amarelo-de-estilo-industrial_469558-56771.jpg?semt=ais_hybrid",
+    },
+    {
+      bgColor: "bg-red-600/15 backdrop-blur-md",
+      title: "🍉 Peças Vermelhas",
+      description: [
+        "As peças vermelhas são compostas por 4 unidades, sendo elas:",
+        "⚡ 2 peças de metal pequenas",
+        "⚡ 2 peças de plástico grandes",
+      ],
+      image:
+        "https://img.freepik.com/fotos-premium/resumo-de-fundo-vermelho-circular_8466-2.jpg?semt=ais_hybrid",
+    },
+    {
+      bgColor: "bg-blue-600/15 backdrop-blur-md",
+      title: "🫐 Peças Azuis",
+      description: [
+        "As peças azuis são compostas por 4 unidades, sendo elas:",
+        "⚡ 2 peças de plástico pequenas",
+        "⚡ 2 peças de metal médias",
+      ],
+      image:
+        "https://img.freepik.com/fotos-gratis/fundo-texturizado-abstrato_1258-30515.jpg",
+    },
+    {
+      bgColor: "bg-green-600/15 backdrop-blur-md",
+      title: "🍋‍🟩 Peças Verdes",
+      description: [
+        "As peças verdes são compostas por 4 unidades, sendo elas:",
+        "⚡ 2 peças de plástico médias",
+        "⚡ 2 peças de metal grandes",
+      ],
+      image:
+        "https://img.freepik.com/fotos-premium/um-fundo-verde-com-um-fundo-verde-que-diz_867255-192.jpg",
+    },
+  ];
+
+  const [container, setContainer] = useState<HTMLElement | null>(null);
+
   return (
-    <main className="sm:ml-14 p-4">
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+    <main className="sm:ml-14 p-4 min-h-screen">
+      <section className="gap-4 h-full">
+        <div
+          className="h-[620px] overflow-auto"
+          ref={(node) => setContainer(node)}
+        >
+          <StackingCards
+            totalCards={cards.length}
+            scrollOptons={{ container: { current: container } }}
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 200 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.5 }}
+              className="relative font-calendas h-[620px] w-full z-10 text-2xl md:text-7xl font-bold uppercase flex justify-center items-center text-[#b3240e] whitespace-pre gap-4"
+            >
+              <MoveDown className="w-8 h-8 md:w-20 md:h-20" />
+              Veja os Nossos Tipos!
+              <MoveDown className="w-8 h-8 md:w-20 md:h-20" />
+            </motion.div>
+            {cards.map(({ bgColor, description, image, title }, index) => {
+              return (
+                <StackingCardItem
+                  key={index}
+                  index={index}
+                  className="h-[620px]"
+                >
+                  <div
+                    className={cn(
+                      bgColor,
+                      "h-[80%] sm:h-[70%] flex-col sm:flex-row gap-8 aspect-video px-8 py-10 flex w-11/12 rounded-3xl mx-auto relative backdrop-filter"
+                    )}
+                  >
+                    <div className="space-y-2 flex-1">
+                      <h3 className="font-bold text-2xl mb-5">{title}</h3>
+                      {Array.isArray(description)
+                        ? description.map((line, i) => (
+                            <p key={i} className="text-xl">
+                              {line}
+                            </p>
+                          ))
+                        : description}
+                    </div>
+                    <div className="w-full sm:w-1/2 rounded-xl aspect-video relative overflow-hidden">
+                      <img src={image} alt={title} className="object-cover" />
+                    </div>
+                  </div>
+                </StackingCardItem>
+              );
+            })}
+            <div className="w-full h-80 relative overflow-hidden">
+              <h2 className="absolute bottom-0 left-0 translate-y-1/3 sm:text-[192px] text-[80px] text-[#b3240e] font-calendas">
+                SENAI
+              </h2>
+            </div>
+          </StackingCards>
+        </div>
+      </section>
     </main>
   );
 }
